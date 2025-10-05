021ec86983851e20cee57268aac5a036d588bfbf
Oleksandr Vlasov <drumlife182@gmail.com>
2025-10-05 21:34:55 +0200

    feat: started working with new entity

--- src/2_pages/tutorials/model.ts
+++ src/2_pages/tutorials/model.ts
@@ -1,1 +1,59 @@
-export const store = null
+import { TTrack } from 'entity/track/model';
+import { atom } from 'nanostores';
+
+const tutorials = [
+  {
+    id: 'lift-off-part1',
+    title: 'Lift-off I: Basics',
+    description: 'Learn the basics of React, the most popular front-end library.',
+    imageUrl:
+      'https://res.cloudinary.com/apollographql/image/upload/v1702928093/odyssey/course-assets-new-brand/LO_course_thumb_beige_ttvtz4.svg',
+    trackType: 'BEGINNER',
+  },
+  {
+    id: 'lift-off-part2',
+    title: 'TypeScript',
+    description:
+      'Learn TypeScript, a typed superset of JavaScript that compiles to plain JavaScript.',
+    imageUrl:
+      'https://res.cloudinary.com/apollographql/image/upload/v1727893429/odyssey/centered-connector_lokjwy.svg',
+    trackType: 'BEGINNER',
+  },
+  {
+    id: 'lift-off-part3',
+    title: 'TypeScript',
+    description:
+      'Learn TypeScript, a typed superset of JavaScript that compiles to plain JavaScript.',
+    imageUrl:
+      'https://res.cloudinary.com/apollographql/image/upload/v1727893429/odyssey/centered-connector_lokjwy.svg',
+  },
+  {
+    id: 'lift-off-part4',
+    title: 'TypeScript',
+    description:
+      'Learn TypeScript, a typed superset of JavaScript that compiles to plain JavaScript.',
+    imageUrl:
+      'https://res.cloudinary.com/apollographql/image/upload/v1727893429/odyssey/centered-connector_lokjwy.svg',
+    trackType: 'BEGINNER',
+  },
+  {
+    id: 'lift-off-part5',
+    title: 'TypeScript',
+    description:
+      'Learn TypeScript, a typed superset of JavaScript that compiles to plain JavaScript.',
+    imageUrl:
+      'https://res.cloudinary.com/apollographql/image/upload/v1727893429/odyssey/centered-connector_lokjwy.svg',
+    trackType: 'BEGINNER',
+  },
+  {
+    id: 'lift-off-part5',
+    title: 'TypeScript',
+    description:
+      'Learn TypeScript, a typed superset of JavaScript that compiles to plain JavaScript.',
+    imageUrl:
+      'https://res.cloudinary.com/apollographql/image/upload/v1727893429/odyssey/centered-connector_lokjwy.svg',
+    trackType: 'BEGINNER',
+  },
+];
+
+export const $tutorials = atom<TTrack[]>(tutorials);
---
+++ src/5_entities/track/model.ts
@@ -0,0 +1,9 @@
+export const store = null;
+
+export type TTrack = {
+  id: string;
+  title: string;
+  description: string;
+  imageUrl: string;
+  trackType?: string;
+};
